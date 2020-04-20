1596aa860035d1e2837ab9ce1735257d57ca2446
commandnetwork <akitama.chabudai@gmail.com>
2020-04-20 18:21:18 +0900

    v2.1.62

--- commandnetworkbot.py
+++ commandnetworkbot.py
@@ -9,31 +9,14 @@ import psutil
 import random
 
 from discord.ext import commands
-#from .config import Config, ConfigDefaults
 
 #変更場所
 token = 'NjgwNzAwMzc4OTI4NjQ0MTE3.XlGwuA.gsmJ-n7-TItMUUuNZM8OAANGlZw'
-bungeest2 = '>>> **BungeeCord** (Proxy Host) \n `Server is Offline`'
-bungeest = '>>> **BungeeCord** (Proxy Host) \n `Server is Online`'
-lobbyst2 = '>>> **Lobby Server** (BungeeCord Lobby) \n `Server is Offline`'
-lobbyst = '>>> **Lobby Server** (BungeeCord Lobby) \n `Server is Online`'
-skyblockst2 = '>>> **The Unusual SkyBlock v12.0.9 Server** (Spigot 1.10.2 Server) \n `Server is Offline`'
-skyblockst = '>>> **The Unusual SkyBlock v12.0.9 Server** (Spigot 1.10.2 Server) \n `Server is Online`'
-commandst2 = '>>> **Command Server** (Spigot 1.12.2 Server) \n `Server is Offline`'
-commandst = '>>> **Command Server** (Spigot 1.12.2 Server) \n `Server is Online`'
-survivalst2 = '>>> **Survival Server** (Spigot 1.15.2 Server) \n `Server is Offline`'
-survivalst = '>>> **Survival Server** (Spigot 1.15.2 Server) \n `Server is Online`'
-athleticst2 = '>>> **Athletic/Parkour Server** (Spigot 1.12.2 Server) \n `Server is Offline`'
-athleticst = '>>> **Athletic/Parkour Server** (Spigot 1.12.2 Server) \n `Server is Online`'
-pvpst2 = '>>> **PvP Server** (Spigot 1.8 Server) \n `Server is Offline`'
-pvpst = '>>> **PvP Server** (Spigot 1.8 Server) \n `Server is Online`'
-modskyblockst2 = '>>> **Skyblock in mod Server** (Forge 1.12.2 Server) \n `Server is Offline`'
-modskyblockst = '>>> **Skyblock in mod Server** (Forge 1.12.2 Server) \n `Server is Online`'
-twilightforestst2 = '>>> **Twilight Forest Server** (Forge 1.7.10 Server) \n `Server is Offline`'
-twilightforestst = '>>> **Twilight Forest Server** (Forge 1.7.10 Server) \n `Server is Online`'
-build = 'v2.1.60'
-kousinnjyouhou = '```CommandsとStatusとInfoとMusic Botが実装されました```'
-osirase = '```特になし```'
+bungeest2 = '>>> **Command Network** \nIP: akitama.xyz, 60.112.154.214 \n `Server is Offline`'
+bungeest = '>>> **Command Network** \nIP: akitama.xyz, 60.112.154.214 \n `Server is Online`'
+build = 'v2.1.62'
+kousinnjyouhou = '```Music Bot導入不可能だったためBot作成者オンライン時のみしか使えません。```'
+osirase = '```一部機能に修正・削除があります```'
 updatelog = 'f'
 
 #help日本語化
@@ -42,8 +25,9 @@ class JapaneseHelpCommand(commands.DefaultHelpCommand):
         super().__init__()
         self.commands_heading = ""
         self.no_category = "コマンド"
-        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
-
+        self.command_attrs["help"] = "\n: コマンド一覧と簡単な説明を表示"
+        self.command_attrs["play"] = "\n: Bot作成者がオンラインのときに使用できます。"
+    
     def get_ending_note(self):
         return (f"各コマンドの説明: s!help <コマンド名>\n"
                 f"各カテゴリの説明: s!help <カテゴリ名>\n")
@@ -66,7 +50,9 @@ mode = 1
 client = discord.Client()
 bot = commands.Bot(command_prefix='s!', help_command=JapaneseHelpCommand())
 mem = psutil.virtual_memory()
+cpu = psutil.cpu_percent(interval=1)
 randomint = random.randint
+randomcho = random.choice
 
 #起動時の処理
 @bot.event
@@ -77,167 +63,91 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name=customstatus))
 
 @bot.command()
-async def st(ctx, *, arg):
-    """Command Networkのサーバーステータスを表示"""
-    if arg == 'BungeeCord' or 'bungeecord':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25565))
-        if bungeeport == 0:
-            await ctx.send(bungeest)
-        else:
-            await ctx.send(bungeest2)
-    if arg == 'Lobby' or 'lobby':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25566))
-        if bungeeport == 0:
-            await ctx.send(lobbyst)
-        else:
-            await ctx.send(lobbyst2)
-    if arg == 'Skyblock''skyblock':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25567))
-        if bungeeport == 0:
-            await ctx.send(skyblockst)
-        else:
-            await ctx.send(skyblockst2)
-    if arg == 'Command''command':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25568))
-        if bungeeport == 0:
-            await ctx.send(commandst)
-        else:
-            await ctx.send(commandst2)
-    if arg == 'Survival''survival':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25569))
-        if bungeeport == 0:
-            await ctx.send(survivalst)
-        else:
-            await ctx.send(survivalst2)
-    if arg == 'Athletic''athletic':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25570))
-        if bungeeport == 0:
-            await ctx.send(athleticst)
-        else:
-            await ctx.send(athleticst2)
-    if arg == 'PvP''pvp':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25571))
-        if bungeeport == 0:
-            await ctx.send(pvpst)
-        else:
-            await ctx.send(pvpst2)
-    if arg == 'Mod Skyblock''mod skyblock':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25572))
-        if bungeeport == 0:
-            await ctx.send(modskyblock)
-        else:
-            await ctx.send(modskyblock2)
-    if arg == 'Twilight Forest''twilight forest':
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        bungeeport = sock.connect_ex(("60.112.154.214",25573))
-        if bungeeport == 0:
-            await ctx.send(twilightforestst)
-        else:
-            await ctx.send(twilightforestst2)
+async def st(ctx):
+    """\n: Command Networkのサーバーステータスを表示"""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    bungeeport = sock.connect_ex(("60.112.154.214",25565))
+    if bungeeport == 0:
+        await ctx.send(bungeest)
     else:
-        await ctx.send('>>> **Info**\n `BungeeCord, Lobby, Skyblock, Command, Survival, Athletic, PvP, Mod Skyblock, Twilight Forest`\nのどれかを入力してください。\n (全部の文字が小文字でも反応します。誤字は反応しません。)')
+        await ctx.send(bungeest2)
 
 @bot.command()
-async def check(ctx, arg):
-    """サーバーアドレスを入れるとオンラインか確認できます。"""
+async def status(ctx):
+    """\n: Command Networkのサーバーステータスを表示"""
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    checkport = sock.connect_ex((arg,25565))
+    bungeeport = sock.connect_ex(("60.112.154.214",25565))
+    if bungeeport == 0:
+        await ctx.send(bungeest)
+    else:
+        await ctx.send(bungeest2)
+
+@bot.command()
+async def check(ctx, arg, arg2):
+    """<サーバーアドレス> <ポート>\n: サーバーアドレスを入れるとオンラインか確認できます。"""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    checkport = sock.connect_ex((arg,int(arg2)))
     if checkport == 0:
         await ctx.send('チェックしたサーバーは現在開いています。')
     else:
         await ctx.send('チェックしたサーバーは現在開いていません。')
 
 @bot.command()
-async def pick(ctx, arg, arg2, arg3):
-    """タイマー"""
-    if arg3 == 'none':
-        a7194 = randomint(1, 3)
-        if a7194 == 1:
-            await ctx.send(arg)
-        if a7194 == 2:
-            await ctx.send(arg2)
-        if a7194 == 3:
-            await ctx.send(arg3)
-    else:
-        a7194 = randomint(1, 2)
-        if a7194 == 1:
-            await ctx.send(arg)
-        if a7194 == 2:
-            await ctx.send(arg2)
+async def pick(ctx, arg, arg2):
+    """<1つ目の語句> <2つ目の語句>\n: 選択された語句からどちらかを選ぶ"""
+    a6790135 = [ arg, arg2 ]
+    await ctx.send(randomcho(a6790135))
 
 @bot.command()
 async def random(ctx, arg, arg2):
+    """<始めの数字> <終わりの数字>\n: はじめと終わりの数字の間でランダムな数字を生成します。"""
     await ctx.send(randomint(int(arg),int(arg2)))
 
 @bot.command()
 async def say(ctx, *, arg):
+    """<言わせたい文字>\n: 言わせたい文字の部分をいいます。"""
     await ctx.send(arg)
 
 @bot.command()
-async def mode(ctx):
-    """チャットモードに変更します。"""
-    await ctx.send('チャットモードに変更しました。')
-    mode = 1
-    print('Quit Command: ecm')
-    while mode == 1:
-        sendmessage = input()
-        await ctx.send(sendmessage)
-        if sendmessage == 'ecm':
-            print('Change Status Check Mode')
-            continue
-    await ctx.send('通常モードに変更しました。')
-
-
-@bot.command()
 async def info(ctx):
-    """起動時のメッセージを再度表示します。"""
+    """\n: このBotの情報を表示します。"""
     await ctx.send(startstatus)
 
 @bot.command()
-async def status(ctx, *, arg):
-    """ステータスを変更します。"""
+async def information(ctx):
+    """\n: このBotの情報を表示します。"""
+    await ctx.send(startstatus)
+
+@bot.command()
+async def activity(ctx, *, arg):
+    """<表示させたい部分>\n: アクティビティを表示させたい部分を追加して変更します。"""
     a1048 = arg + sikiri + customstatus
     await bot.change_presence(activity=discord.Game(name=a1048))
 
 @bot.command()
-async def shutdown(ctx):
-    await ctx.send('Botをシャットダウンします。')
-    sys.exit(code)
-
-@bot.command()
-async def commands(ctx, arg):
-    log.info("This is Developing Event")
-
-@bot.command()
 async def invite(ctx):
+    """\n: 招待コードを表示します。"""
     await ctx.send('Command Network: https://discord.gg/6yETYTK\nPlease read rules')
 
 @bot.command()
-async def performance(ctx, arg):
-    if arg == 'Memory' or 'memory' or 'mem' or 'Mem':
-        a173013 = str(mem.percent) + '%'
-        await ctx.send(a173013)
+async def pfm(ctx, arg):
+    """\n: パフォーマンス(ホストコンピューターの状態)を表示します。"""
+    a173013 = 'Memory: ' + str(mem.percent) + '%' + ' (' + str(mem.used / 1024 / 1024) + ' / ' + str(mem.total / 1024 / 1024) + 'MB' + ')'
+    await ctx.send(a173013)
+    a124897 = 'CPU: ' + str(cpu) + '%'
+    await ctx.send(a124897)
 
 @bot.command()
 async def neta(ctx):
-    a6193 = randomint(1, 3)
-    if a6193 == 1:
-        await ctx.send(file=discord.File('neta1.mp4'))
-    if a6193 == 2:
-        await ctx.send(file=discord.File('neta2.mp4'))
-    if a6193 == 3:
-        await ctx.send(file=discord.File('neta3.png'))
+    """\n: ネタをランダムで選び表示します。"""
+    a79424 = [ neta1.mp4, neta2.mp4, neta3.png ]
+    await ctx.send(file=discord.File(randomcho(a79424)))
 
 @bot.command()
-async def calc(ctx, arg):
-    await ctx.send(arg)
+async def music(ctx):
+    """\n: Botの作成者が現時点で気に入っている曲のYouTubeリンクをチャンネルにアップロードします。"""
+    await ctx.send('PSYQUI, Mo∀ - Rainbow Dream')
+    await ctx.send('https://www.youtube.com/watch?v=BCM423NDOE0')
+    await ctx.send('(Music Botに挿入済, s!play Rainbow Dream.mp3)')
 
 bot.run(token)
