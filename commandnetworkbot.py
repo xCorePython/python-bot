f8a0d8dd1aca52a582e03aff2d7f391cc75c68cf
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-10-04 19:56:41 +0900

    v4.01.15

--- commandnetworkbot.py
+++ commandnetworkbot.py
@@ -9,7 +9,7 @@ importtime = float(now.strftime("0.%f")) + int(now.second) + int(int(now.day) * 
 sys_token = 'NzYxOTI5NDgxNDIxOTc5NjY5.X3hwIA.ItlW0Q2Fej-OyNdbfUKO2czZQvk'
 sys_token2 = 'NzYwNDkwNjYwNDQzODQ4NzM0.X3M0Hg.lTDx_AvmNNr1spqwUo1wqetaVlM'
 sys_token3 = 'NjgwOTAxMTEyOTA3NTYzMDcx.XxLShg.NdGG5gd8gQ9_GGTqomBBqSfRC08'
-sys_version = 'v4.01.14'
+sys_version = 'v4.01.15'
 ready_log = '複数のコマンドを修正'
 ready_log2 = 'いろんなコマンドを追加'
 ready_info = 'バグがある可能性があります。`Cn!report <バグ内容>`で報告してください！'
@@ -179,17 +179,32 @@ def now_date(mode, location):
         return a01.strftime("%Y/%m/%d %H:%M:%S.%f")
 
 def reverse(time):
-    if hour < 3600:
-        minute = str(time / 60)
+    if time < 3600:
+        minute = str(int(time / 60))
         second = str(int(time - int(minute * 60)))
-        uptime = minute + ':' + second
-        return uptime
+        if second < 10:
+            uptime = minute + ':0' + second
+            return uptime
+        else:
+            uptime = minute + ':' + second
     else:
-        hour = str(time / 3600)
-        minute = str(time - int(hour * 3600))
+        hour = str(int(time / 3600))
+        minute = str(int(time - int(hour * 3600)))
         second = str(int(time - int(hour * 3600) - int(minute * 60)))
-        uptime = hour + ':' + minute + ':' + second
-        return uptime
+        if minute < 10:
+            if second < 10:
+                uptime = hour + ':0' + minute + ':0' + second
+                return uptime
+            else:
+                uptime = hour + ':0' + minute + ':' + second
+                return uptime
+        else:
+            if second < 10:
+                uptime = hour + ':' + minute + ':0' + second
+                return uptime
+            else:
+                uptime = hour + ':' + minute + ':' + second
+                return uptime
 
 def savetime():
     now = datetime.datetime.utcnow()
