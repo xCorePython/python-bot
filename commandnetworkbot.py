4ebca1c30f3e426e510d47675bab3fc4120ade98
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-11-09 22:28:36 +0900

    Update commandnetworkbot.py

--- commandnetworkbot.py
+++ commandnetworkbot.py
@@ -446,7 +446,7 @@ async def on_ready():
     	info = links[n].split('watch?v=')[1]
     	conv(info)
     n = 0
-    await client.get_channel(vcch).guild.voice_client.play(discord.FFmpegOpusAudio('{0}.opus'.format(queue[n]), bitrate=320))
+    client.get_channel(vcch).guild.voice_client.play(discord.FFmpegOpusAudio('{0}.opus'.format(queue[n]), bitrate=320))
     start = now_date('off', 9)
     while sys_loop == 1:
     	time = float(now_date('off', 9) - start)
@@ -454,7 +454,7 @@ async def on_ready():
     	await status('Time : {} / {} | {}'.format(reverse(time), audio , sys_activity))
     	try:
     		n = n + 1
-    		await client.get_channel(vcch).guild.voice_client.play(discord.FFmpegOpusAudio('{0}.opus'.format(queue[n]), bitrate=320))
+    		client.get_channel(vcch).guild.voice_client.play(discord.FFmpegOpusAudio('{0}.opus'.format(queue[n]), bitrate=320))
     		start = now_date('off', 9)
     	except:
     			if n > len(queue):
