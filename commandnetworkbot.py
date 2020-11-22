cabea8393353c700c9f3a5410fb748e0d91db088
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-11-22 19:27:23 +0900

    Update commandnetworkbot.py

--- commandnetworkbot.py
+++ commandnetworkbot.py
@@ -358,9 +358,7 @@ async def commands(command, message):
 		    file=discord.File('uploader/{}'.format(arg)))
 	elif command == 'downloader':
 		arg = message.content.split(' ')
-		info = youtube_dl.YoutubeDL().extract_info(
-		    arg[1], download=False, process=False)
-		link = 'https://youtu.be/{}'.format(info['id'])
+		link = arg[1]
 		if len(arg) == 2:
 			ydl = youtube_dl.YoutubeDL(ydl_opts2)
 			await message.channel.send('Downloading... (128kbps)')
@@ -374,6 +372,7 @@ async def commands(command, message):
 			await message.channel.send(
 			    file=discord.File('youtube/{0}.mp3'.format(info_dict['id'])))
 		if arg[2] == 'high':
+			info = youtube_dl.YoutubeDL().extract_info(link, download=False, process=False)
 			thumbnail = info['thumbnails'][len(info['thumbnails']) - 1]['url']
 			url = 'https://www.320youtube.com/v11/watch?v={}'.format(
 			    info['id'])
